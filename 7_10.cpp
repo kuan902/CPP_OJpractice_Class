bc08560d3986e5ebf34d9cf3e0b3a7828d7441d3
Kuan <1772017002@qq.com>
2026-03-29 07:38:47 +0000

    add 7_10.cpp

---
+++ 7_10.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+
+class Point
+{
+    public:
+    double x,y;
+    Point():x(0),y(0) {}
+    Point(double a,double b):x(a),y(b) {}
+    double r()
+    {
+        return sqrt(x*x+y*y);
+    } 
+    double distance(const Point& p)
+    {
+        double dx=x-p.x;
+        double dy=y-p.y;
+        return sqrt(dx*dx+dy*dy);
+    }
+    Point relative(const Point& p)
+    {
+        return Point(x-p.x,y-p.y);
+    }
+    bool is_above_left(const Point& p)
+    {
+        return(x<p.x)&&(y>p.y);
+    }
+
+};
+int main() {
+	double x2, y2, x3, y3;
+	cin >> x2 >> y2 >> x3 >> y3;
+	Point p1, p2(x2, y2), p3(x3, y3);
+	cout << fixed << setprecision(2) << p2.r() << endl;
+	cout << "The distance between p3 and p2 is " << fixed << setprecision(2) << p3.distance(p2) << endl;
+	Point p4 = p2.relative(p3);
+	cout << fixed << setprecision(2) << "p4:(" << p4.x << "," << p4.y << ")" << endl;
+	cout << "Is p2 is above left of p3?: " << p2.is_above_left(p3) << endl;
+	return 0;
+}
